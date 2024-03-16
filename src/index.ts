74144a1cf312cea7f1e4d057451e1ae7d35be504
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-03-16 21:33:47 +0300

    feat: add v1 elysia support  https://github.com/elysiajs/elysia/issues/513

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,29 @@
-import { Elysia } from "elysia"
-import { Options, Packr } from "msgpackr"
+import { Elysia } from "elysia";
+import { type Options, Packr } from "msgpackr";
 
-const DEFAULT_MIME_TYPE = "application/x-msgpack"
+const DEFAULT_MIME_TYPE = "application/x-msgpack";
 
 // TODO: worry about onError serialize to msgpack
 export function msgpack(options: Options & { mimeType?: string } = {}) {
-    const packr = new Packr(options)
+	const packr = new Packr(options);
 
-    return new Elysia({
-        name: "elysia-msgpack",
-        seed: options,
-    })
-        .onParse(async ({ request }, contentType) => {
-            if (contentType === (options.mimeType ?? DEFAULT_MIME_TYPE))
-                return packr.unpack(new Uint8Array(await request.arrayBuffer()))
-        })
-        .mapResponse(({ headers, response }) => {
-            if (
-                headers.accept?.includes(
-                    options.mimeType ?? DEFAULT_MIME_TYPE,
-                ) &&
-                response
-            )
-                return new Response(packr.pack(response), {
-                    headers: {
-                        "content-type": options.mimeType ?? DEFAULT_MIME_TYPE,
-                    },
-                })
-        })
+	return new Elysia({
+		name: "elysia-msgpack",
+		seed: options,
+	})
+		.onParse({ as: "global" }, async ({ request }, contentType) => {
+			if (contentType === (options.mimeType ?? DEFAULT_MIME_TYPE))
+				return packr.unpack(new Uint8Array(await request.arrayBuffer()));
+		})
+		.mapResponse({ as: "global" }, ({ headers, response }) => {
+			if (
+				headers.accept?.includes(options.mimeType ?? DEFAULT_MIME_TYPE) &&
+				response
+			)
+				return new Response(packr.pack(response), {
+					headers: {
+						"content-type": options.mimeType ?? DEFAULT_MIME_TYPE,
+					},
+				});
+		});
 }
