de97a3950bd01b50233a8efadfd1b95b5dccd902
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-06-24 20:21:35 +0300

    fix: some fixes for https://github.com/kravetsone/elysia-msgpack/pull/3

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,28 @@ import { type Options, Packr } from "msgpackr";
 const DEFAULT_MIME_TYPE = "application/x-msgpack";
 
 export interface ElysiaMsgpackOptions extends Options {
-	mimeType?: string
-	forceEnable?: boolean
+	mimeType?: string;
+	force?: boolean;
 }
 
-// TODO: worry about onError serialize to msgpack
-export function msgpack(opts: ElysiaMsgpackOptions = {}) {
-	const packr = new Packr(opts);
+export function msgpack(options: ElysiaMsgpackOptions = {}) {
+	const packr = new Packr(options);
 
-	opts.mimeType ??= DEFAULT_MIME_TYPE
+	const mimeType = options.mimeType ?? DEFAULT_MIME_TYPE;
 
 	return new Elysia({
 		name: "elysia-msgpack",
 		seed: options,
 	})
 		.onParse({ as: "global" }, async ({ request }, contentType) => {
-			if (contentType === opts.mimeType)
+			if (contentType === mimeType)
 				return packr.unpack(new Uint8Array(await request.arrayBuffer()));
 		})
 		.mapResponse({ as: "global" }, ({ headers, response }) => {
-			if (
-				response && 
-				(opts.forceEnable || headers.accept?.includes(opts.mimeType))
-			) {
+			if (response && (options.force || headers.accept?.includes(mimeType))) {
 				return new Response(packr.pack(response), {
 					headers: {
-						"content-type": opts.mimeType,
+						"content-type": mimeType,
 					},
 				});
 			}
