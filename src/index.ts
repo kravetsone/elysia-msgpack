e4696985f72fb7a751b140ea16fd4ef78db1c94e
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-06-24 20:28:11 +0300

    feat: add life-cycle options

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
-import { Elysia } from "elysia";
+import { Elysia, type LifeCycleType } from "elysia";
 import { type Options, Packr } from "msgpackr";
 
 const DEFAULT_MIME_TYPE = "application/x-msgpack";
 
-export interface ElysiaMsgpackOptions extends Options {
+export interface ElysiaMsgpackOptions<Type extends LifeCycleType = "scoped">
+	extends Options {
 	mimeType?: string;
 	force?: boolean;
+	as?: Type;
 }
 
-export function msgpack(options: ElysiaMsgpackOptions = {}) {
+export function msgpack<Type extends LifeCycleType>(
+	options: ElysiaMsgpackOptions<Type> = {},
+) {
 	const packr = new Packr(options);
 
 	const mimeType = options.mimeType ?? DEFAULT_MIME_TYPE;
+	const as = (options.as ?? "scoped") as Type;
 
 	return new Elysia({
 		name: "elysia-msgpack",
 		seed: options,
 	})
-		.onParse({ as: "global" }, async ({ request }, contentType) => {
+		.onParse({ as }, async ({ request }, contentType) => {
 			if (contentType === mimeType)
 				return packr.unpack(new Uint8Array(await request.arrayBuffer()));
 		})
-		.mapResponse({ as: "global" }, ({ headers, response }) => {
+		.mapResponse({ as }, ({ headers, response }) => {
 			if (response && (options.force || headers.accept?.includes(mimeType))) {
 				return new Response(packr.pack(response), {
 					headers: {
