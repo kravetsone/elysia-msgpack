e0875078bdfcffb2e61b63c2bf5d981f3d9e9fed
zoto_ff <142039751+zoto-ff@users.noreply.github.com>
2024-06-24 07:54:00 +0300

    feat: add forceEnable option

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,35 @@ import { type Options, Packr } from "msgpackr";
 
 const DEFAULT_MIME_TYPE = "application/x-msgpack";
 
+export interface ElysiaMsgpackOptions extends Options {
+	mimeType?: string
+	forceEnable?: boolean
+}
+
 // TODO: worry about onError serialize to msgpack
-export function msgpack(options: Options & { mimeType?: string } = {}) {
-	const packr = new Packr(options);
+export function msgpack(opts: ElysiaMsgpackOptions = {}) {
+	const packr = new Packr(opts);
+
+	opts.mimeType ??= DEFAULT_MIME_TYPE
 
 	return new Elysia({
 		name: "elysia-msgpack",
 		seed: options,
 	})
 		.onParse({ as: "global" }, async ({ request }, contentType) => {
-			if (contentType === (options.mimeType ?? DEFAULT_MIME_TYPE))
+			if (contentType === opts.mimeType)
 				return packr.unpack(new Uint8Array(await request.arrayBuffer()));
 		})
 		.mapResponse({ as: "global" }, ({ headers, response }) => {
 			if (
-				headers.accept?.includes(options.mimeType ?? DEFAULT_MIME_TYPE) &&
-				response
-			)
+				response && 
+				(opts.forceEnable || headers.accept?.includes(opts.mimeType))
+			) {
 				return new Response(packr.pack(response), {
 					headers: {
-						"content-type": options.mimeType ?? DEFAULT_MIME_TYPE,
+						"content-type": opts.mimeType,
 					},
 				});
+			}
 		});
 }
